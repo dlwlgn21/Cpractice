d78f584970eec4d058a1b36ac7dc67ea6c149fc3
DESKTOP-D4V9PEA\USER <dlwlgn213@gmail.com>
2022-01-06 22:19:00 +0900

    fix error

--- 0106_is_overrap/is_overrap.c
+++ 0106_is_overrap/is_overrap.c
@@ -7,7 +7,7 @@ int is_overrap(int* nums1, const size_t length1, int* nums2, const size_t length
 	nums1 + length1 > nums2 :
 	nums2 + length2 > nums1;
 	/* 1000 <= 1004 ? 즉, num1가 num2보다 왼쪽에 있으면 */
-	/* 1000 + 5 = 1014 > 1004 겹침!!! */
-	/* 1000 <= FFC 즉, num2가 num1보다 왼쪽에 있으면 */
-	/* FFC + 3 = 1008  > num1 이면 겹침!!*/
+	/* 1000 + 5(0x14) = 1014 > 1004 겹침!!! */
+	/* 1000 > FFC 즉, num2가 num1보다 왼쪽에 있으면 */
+	/* FFC + 3(0x0C) = 1008  > num1 이면 겹침!!*/
 }
